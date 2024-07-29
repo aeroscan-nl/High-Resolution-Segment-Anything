c56d4696f93cbb01320a3d84a1e42c58e53f447f
DylanHasperhoven <dylan@aeroscan.nl>
2024-07-29 17:02:13 +0200

    Add data preprocessing and insert previous logits into prompt

--- models/segmentors/promptseg.py
+++ models/segmentors/promptseg.py
@@ -1,29 +1,26 @@
-import warnings
-
 import torch
 from mmseg.registry import MODELS
 
-from engine.timers import Timer
 from .base import BaseClickSegmentor
-import os
-import cv2
 
 class SegmentorPrompt:
-    def __init__(self, image, points=None, boxes=None):
+    def __init__(self, image, points=None, boxes=None, logits=None):
         self.image = image
         self.points = points
         self.boxes = boxes
+        self.logits = logits
 
 
 @MODELS.register_module()
 class PromptSegmentor(BaseClickSegmentor):
     """
-    Segmentor that performs standard forward passes using user prompts .
+    Segmentor that performs standard forward passes using user prompts.
     """
 
     @torch.no_grad()
     def prompted_inference(self, prompt: SegmentorPrompt):
         cfg = self.test_cfg
+        self.eval()
 
         # Encode image 
         resized_padded_inputs = self.resize_and_pad_to_target_size(prompt.image, cfg.target_size)
@@ -32,14 +29,19 @@ class PromptSegmentor(BaseClickSegmentor):
         # Encode image embeddings with prompt
         points = self.resize_coord_to_target_size(
             prompt.points, prompt.image.shape[-2:], cfg.target_size, prompt.image.device)
-        prompt_embeds = self.neck(image_embeds, points, prompt.boxes)
+        prompt_embeds = self.neck(
+            image_embeds=image_embeds,
+            points=points,
+            boxes=prompt.boxes,
+            prev_logits=prompt.logits)
         
-        # Decode image + prompt embeddings
+        # Decode image + prompt embeddings and post-process mask
         logits = self.decode_head(prompt_embeds, mode='single mask')
         mask = self.interpolate(logits, resized_padded_inputs.shape[-2:])
         mask = self.crop_and_resize_to_original_size(
             mask, prompt.image.shape[-2:], cfg.target_size)
-        # mask = mask > 0.0
+        mask = mask > 0.0
         mask = (mask.squeeze().clamp(0, 255) * 255).to(torch.uint8)
         mask = mask.squeeze().detach().cpu().numpy()
-        return mask
+        
+        return mask, logits
--- tools/inference.py
+++ tools/inference.py
@@ -1,10 +1,9 @@
 import argparse
-import time
 import cv2
-import numpy as np
 import torch
 
 from mmengine.config import Config
+from mmengine.runner import set_random_seed
 from mmseg.registry import MODELS
 
 from models.segmentors.promptseg import SegmentorPrompt
@@ -21,32 +20,64 @@ def parse_args():
 
 
 def build_model(cfg, checkpoint_path):
+    preprocessor = MODELS.build(cfg.data_preprocessor)
     checkpoint = torch.load(checkpoint_path, map_location='cpu')
     model = MODELS.build(cfg.model)
     model.load_state_dict(checkpoint)
     model.to("cuda")
-    return model
+    return model, preprocessor
+
+
+def load_image(img_path, preprocessor):
+    image = cv2.imread(img_path)
+    image = image.transpose((2, 0, 1))
+    image = torch.tensor(image).to("cuda")
+    data = preprocessor({'inputs': [image], 'data_samples': []})
+    image = data['inputs'][0]
+    image = image[None, :, :, :].to("cuda")
+    return image
 
 
 def main():
     args = parse_args()
     cfg = Config.fromfile(args.config)
-    model = build_model(cfg, args.checkpoint)
+    set_random_seed(cfg.randomness.seed)
     
-    image = cv2.imread(args.image)
-    image = image.transpose((2, 0, 1))
-    image = image[None, :, :, :]
-    image = image.astype(np.float32) / 255.0
-    image = torch.tensor(image).to("cuda")
-    # TODO: Might need pre-processing still    
+    model, preprocessor = build_model(cfg, args.checkpoint)
+    image = load_image(args.image, preprocessor)
+    points = [
+        (99, 118, 'positive'),
+        (309, 276, 'negative'),
+        (156, 323, 'negative'),
+        (440, 189, 'negative'),
+        (361, 139, 'positive'),
+        (371, 115, 'negative'),
+        (366, 121, 'positive'),
+        (357, 114, 'positive'),
+        (358, 112, 'positive'),
+        (67, 297, 'negative'),
+        (360, 115, 'negative'),
+        (371, 113, 'negative'),
+        (365, 124, 'positive'),
+        (365, 118, 'positive'),
+        (369, 121, 'positive'),
+        (371, 117, 'negative'),
+        (368, 124, 'positive'),
+        (372, 114, 'negative'),
+        (374, 89, 'positive'),
+        (364, 125, 'positive')
+    ]
     
-    prompt = SegmentorPrompt(
-        image=image,
-        points=[(130, 600, 'positive')],
-        boxes=None
-    )
-    mask = model(prompt=prompt, mode='prompt')
-    cv2.imwrite('mask.png', mask)
+    prev_logits = None
+    for i in range(len(points)):
+        prompt = SegmentorPrompt(
+            image=image,
+            points=points[:i + 1],
+            boxes=None,
+            logits=prev_logits
+        )
+        mask, prev_logits = model(prompt=prompt, mode='prompt')
+        cv2.imwrite(f'mask{i}.png', mask)
 
 
 if __name__ == '__main__':
