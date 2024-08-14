770d77a0fade3e64d0eb748ada76b9b7d0d815ab
DylanHasperhoven <dylan@aeroscan.nl>
2024-08-14 17:05:10 +0200

    Fix setup packages

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='hrsam',
     version='0.1.0',
-    packages=find_packages(include=['high-resolution-segment-anything', 'engine', 'models']),
+    packages=find_packages(exclude=['mmdetection', 'mmsegmentation', 'selective_scan']),
     install_requires=[],
 )
